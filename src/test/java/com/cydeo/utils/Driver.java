7ef839e87da9413d54418251087abc066d5c62be
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-06-22 00:12:41 -0500

    commit swaglab

--- src/test/java/com/cydeo/utils/Driver.java
+++ src/test/java/com/cydeo/utils/Driver.java
@@ -29,12 +29,34 @@ public class Driver {
                     desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
                     desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3");
                     desiredCapabilities.setCapability(MobileCapabilityType.APP, "https://cybertek-appium.s3.amazonaws.com/calculator.apk");
+
+
                     try {
                         url = new URL("http://localhost:4723/wd/hub");
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
                     }
                     driver = new AndroidDriver<>(url, desiredCapabilities);
+                    break;
+
+                case "android-swaglab":
+                    DesiredCapabilities desiredCapabilities2 = new DesiredCapabilities();
+                    desiredCapabilities2.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+                    desiredCapabilities2.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+                    desiredCapabilities2.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+                    desiredCapabilities2.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3");
+                    desiredCapabilities2.setCapability(MobileCapabilityType.APP, "C:\\Users\\civic\\IdeaProjects\\jd-b28\\AppiumAutomation\\Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
+
+                    desiredCapabilities2.setCapability("appPackage", "com.swaglabsmobileapp");
+                    desiredCapabilities2.setCapability("appActivity", "com.swaglabsmobileapp.SplashActivity");
+
+
+                    try {
+                        url = new URL("http://localhost:4723/wd/hub");
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    driver = new AndroidDriver<>(url, desiredCapabilities2);
                     break;
                 case "android-remote":
                     DesiredCapabilities caps = new DesiredCapabilities();
